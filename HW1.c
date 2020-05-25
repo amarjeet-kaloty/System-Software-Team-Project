68d6a374b734bb5058f740db8e2475df37ffa261
ashwinjega <56772146+ashwinjega@users.noreply.github.com>
2020-05-25 14:51:38 -0400

    Update HW1.c

--- HW1.c
+++ HW1.c
@@ -11,14 +11,24 @@ struct instruction
   int l; // lexicographical level
   int m; // depends on opcode.
 };
-struct instruction code[MAX_CODE_LENGTH];
+int code[MAX_CODE_LENGTH];
 int stack[MAX_DATA_STACK_HEIGHT];
 
 struct instruction ir[10000];
 int sp = MAX_DATA_STACK_HEIGHT; //stack pointer
 int bp = sp - 1;  //base pointer
 int pc = 0; //program counter
+int haltflag;
+int base(int l, int base){
 
+	int b1; //find base L levels down
+	b1 = base;
+	while (l > 0){
+		b1 = stack[b1 + 1];
+	  	l--;
+	}
+	return b1;
+}
 
 
 
@@ -37,7 +47,7 @@ void execute()
         sp = sp+1;
         stack[sp] = ir.m;
 
-    }
+
     case 2: //OPR, so m must be evaluated.
         switch(ir.m)
         {
@@ -46,7 +56,7 @@ void execute()
             {
                 sp = bp+1;
                 pc = stack[sp - 4];
-                bp = stackp[sp - 3];
+                bp = stack[sp - 3];
                 break;
 
             }
@@ -94,7 +104,7 @@ void execute()
         case 8: //EQL
             {
                 sp=sp+1;
-                stack[sp] = (((stack[sp == stack[sp-1)]) ? 1 : 0);
+                stack[sp] = ((stack[sp] == stack[sp-1]) ? 1 : 0);
                 break;
             }
         case 9: //NEQ
@@ -109,7 +119,7 @@ void execute()
                 stack[sp] = ((stack[sp] < stack[sp-1]) ? 1 : 0);
                 break;
             }
-        case 11: /LEQ
+        case 11: //LEQ
             {
                 sp=sp+1;
                 stack[sp] = ((stack[sp] <= stack[sp-1]) ? 1 : 0);
@@ -129,6 +139,87 @@ void execute()
             }
         }
         break;
+
+        case 3: // LOD, this should load the value from location M from L levels up and push it to the top.
+            {
+                int l = ir.l;
+                int b;
+                while (l>0)
+                {
+
+                    b = stack[b-1];
+                    l--;
+                }
+                sp = sp - 1;
+                stack[sp] = stack[base(l, b) - ir.m];
+                break;
+            }
+        case 4:  // STO - take the value from the top of the stack and store it at location M from L levels up.
+            {
+                int l = ir.l;
+                int b;
+                while (l>0)
+                {
+
+                    b = stack[b-1];
+                    l--;
+                }
+                stack[base(l, b) - ir.m] = stack[sp];
+                sp = sp + 1;
+
+                break;
+
+            }
+        case 5: // CALL at M
+            {
+                stack[sp-1] = 0;
+                stack[sp-2] = base(ir.l, bp);
+                stack[sp-3] = bp;
+                stack[sp-4] = pc;
+                bp = sp-1;
+                pc = ir.m;
+                break;
+            }
+        case 6: // INC
+            {
+                sp = sp - ir.m;
+                break;
+            }
+        case 7: // JMP
+            {
+                pc = ir.m;
+                break;
+            }
+        case 8: // JPC
+            {
+                if (stack[sp] == 0)
+                {
+                    pc = ir.m;
+                }
+                sp = sp +1;
+                break;
+            }
+        case 9: //SIO
+            {
+                printf("%d", &stack[sp]);
+                sp=sp+1;
+                break;
+
+            }
+        case 10: //SIO 0, 2
+            {
+
+                sp=sp-1;
+                scanf("%d", &stack[sp]);
+                break;
+            }
+        case 11: // SIO 0, 3
+            {
+
+                haltflag=0;
+                break;
+            }
+
 
 
 
