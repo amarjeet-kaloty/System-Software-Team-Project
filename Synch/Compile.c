03aa9e13a4ea38e1a592c2e799037daa8c8d5573
Amarjeet <60272668+amarjeet-kaloty@users.noreply.github.com>
2020-07-08 01:20:45 -0400

    Update Compile.c

--- Synch/Compile.c
+++ Synch/Compile.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+#include "LA.h"
+#include "Parser.h"
+#include "VM.h"
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int lFlag=0;
+    int pFlag=0;
+    int tFlag=0;
+
+    for (i=0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-l") ==0)
+            lFlag=1;
+
+        if (strcmp(argv[i], "-a") ==0)
+            pFlag=1;
+
+        if (strcmp(argv[i], "-v") ==0)
+            tFlag=1;
+    }
+    LA(lFlag);
+    Parser(pFlag);
+    VM(tFlag);
+    return 0;
+}
